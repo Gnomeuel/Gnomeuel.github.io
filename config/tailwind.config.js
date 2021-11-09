10b7dbf8c6b613e9fce5e842978612f57d86f2c0
Sam <61364964+Gnomeuel@users.noreply.github.com>
2021-11-09 20:17:22 +0000

    Add files via upload

---
+++ config/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'Roboto': ['Roboto', 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
